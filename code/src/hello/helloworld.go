29c414039404044a0baa8637033c5e4edb880655
南城 <lampxiezi@163.com>
2019-11-28 14:33:54 +0800

    添加代码

---
+++ code/src/hello/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {  
+	var age int = 14
+	fmt.Println("Hello World",age)
+	age = 29
+	fmt.Println("my age is",age)
+
+	name,sex :="pfinal","男"
+	fmt.Println("my name is", name, "sex is", sex)
+}
