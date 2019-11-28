06d561bc884f4b5e3d4dab26789ef98370fe18e6
南城 <lampxiezi@163.com>
2019-11-28 14:53:00 +0800

    添加代码

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # go-study
 
 Author: PFinal社区
+
+- 学习链接
+
+https://www.yuque.com/ksco/ogg7um/wvfo6h
---
+++ code/src/hello/data_type.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "unsafe"
+func main()  {
+	a :=true
+	b := false
+	fmt.Println("a:", a, "b:", b)
+	c := a && b
+    fmt.Println("c:", c)
+    d := a || b
+	fmt.Println("d:", d)
+	
+	var e int = 89
+	f := 95
+	fmt.Println("value of a is", e, "and b is", f)
+	fmt.Printf("type of a is %T, size of a is %d\n", a, unsafe.Sizeof(e)) //type and size of a
+	fmt.Printf("type of a is %T, size of a is %d\n", a, unsafe.Sizeof(f)) //type and size of a
+}
