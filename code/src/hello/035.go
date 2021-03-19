0a303ee6c3c6ba16a08a1481f12fb3b760dac0e8
LXZ <xuzhu.liu@avazu.net>
2021-03-19 10:26:14 +0800

    更新

--- code/src/hello/035.go
+++ code/src/hello/035.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func min(s ...int) int {
-	
+	if len(s)==0 {
+		return 0
+	}
+
+	min :=s[0]
+	for _,v :=range s {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func main() {
+	x := min(1,2,2,0)
+	fmt.Printf("The mininum is: %d\n", x)
+	slice :=[]int{7,9,3,5,1}
+	fmt.Println(slice)
+	x = min(slice...)
+	fmt.Printf("The minimum in the slice is: %d \n", x)
 }
---
+++ code/src/hello/036.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+
+	// 字符串可以通过 `+` 链接
+	fmt.Println("PFinal" + "南丞")
+
+	// 整数和浮点数
+	fmt.Println("1+1 = ",1+1)
+	fmt.Println(true|| false )
+}
