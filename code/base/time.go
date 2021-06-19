fdbd558674d91a386b2eafb1f2b08a224d1cdc1b
LXZ <xuzhu.liu@avazu.net>
2021-06-19 16:36:05 +0800

    更新

---
+++ code/base/time.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var week time.Duration
+
+func main() {
+	t := time.Now()
+	fmt.Print(t)
+	fmt.Println(t)
+	fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year())
+	t = time.Now().UTC()
+	fmt.Println(t) // Wed Dec 21 08:52:14 +0000 UTC 2011
+	fmt.Println(time.Now()) // Wed Dec 21 09:52:14 +0100 RST 2011
+	// calculating times:
+	week = 60 * 60 * 24 * 7 * 1e9 // must be in nanosec
+	week_from_now := t.Add(time.Duration(week))
+	fmt.Println(week_from_now) // Wed Dec 28 08:52:14 +0000 UTC 2011
+	// formatting times:
+	fmt.Println(t.Format(time.RFC822)) // 21 Dec 11 0852 UTC
+	fmt.Println(t.Format(time.ANSIC)) // Wed Dec 21 08:56:34 2011
+	// The time must be 2006-01-02 15:04:05
+
+}
